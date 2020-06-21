bd0ca34b2ad3abe45db56ff9a046cf0f84633650
tiepnn <tiepnguyenptit@gmail.com>
2020-06-21 14:59:15 +0700

    BUOI 4

---
+++ src/main/java/application/data/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package application.data.repository;
+
+import application.data.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    @Query("select count(p.id) from dbo_product p")
+    long getTotalProducts();
+
+    @Query("SELECT p FROM dbo_product p " +
+            "WHERE (:categoryId IS NULL OR (p.categoryId = :categoryId))" +
+            "AND (:productName IS NULL OR UPPER(p.name) LIKE CONCAT('%',UPPER(:productName),'%'))")
+    Page<Product> getListProductByCategoryOrProductNameContaining(Pageable pageable, @Param("categoryId") Integer categoryId, @Param("productName") String productName);
+
+}
---
+++ src/main/java/application/model/dto/ProductDTO.java
@@ -0,0 +1,88 @@
+package application.model.dto;
+
+import application.extension.CustomDateDeserializer;
+import application.extension.CustomDateSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Date;
+
+public class ProductDTO {
+
+    private int id;
+//    @JsonProperty("categoryId")
+    private int categoryId;
+    private String categoryName;
+    private String name;
+    private String shortDesc;
+    private String mainImage;
+    private Double price;
+
+    @JsonSerialize(using = CustomDateSerializer.class)
+    @JsonDeserialize(using = CustomDateDeserializer.class)
+    private Date createdDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
