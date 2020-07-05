4a8b2439dfcfb2d4fa3943ff83daeb38ee62bbed
tiepnn <tiepnguyenptit@gmail.com>
2020-07-05 16:21:37 +0700

    spring security

--- src/main/java/application/config/WebConfig.java
+++ src/main/java/application/config/WebConfig.java
@@ -11,6 +11,8 @@ import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomize
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
@@ -40,7 +42,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
 
-
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder getPasswordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/application/error/DemoAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package application.error;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+public class DemoAccessDeniedHandler implements AccessDeniedHandler {
+
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse,
+                       AccessDeniedException e) throws IOException {
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/403");
+    }
+}
